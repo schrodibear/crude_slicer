dd6768a73afcd8090e39cb9246747ed06dfbac4c
Mikhail Mandrykin <mandrykin@ispras.ru>
2017-01-30 23:45:10 +0300

    Effect -> Info, Fixpoint as separate module

---
+++ fixpoint.ml
@@ -0,0 +1,59 @@
+(**************************************************************************)
+(*                                                                        *)
+(*  Crude slicer for preprocessing reachability verification tasks        *)
+(*                                                                        *)
+(*  Copyright (C) 2016-2017 Mikhail Mandrykin, ISP RAS                    *)
+(*                                                                        *)
+(**************************************************************************)
+
+open Cil_types
+open Visitor
+
+open Format
+open Common
+
+module type Info = sig
+  module E : sig
+    type t
+
+    val pp : formatter -> t -> unit
+  end
+
+  type t
+
+  val get : t -> Flag.t -> fundec -> E.t
+end
+
+module Make (I : Info) = struct
+  let flag = Flag.create "convergence"
+
+  let rec until_convergence f fi scc =
+    Flag.clear flag;
+    f fi scc;
+    if not (Flag.reported flag) then ()
+    else until_convergence f fi scc
+
+  class type ['a] frama_c_collector = object inherit frama_c_inplace method finish : 'a end
+
+  let visit_until_convergence ~order (v : _ -> _ -> _ #frama_c_collector) fi sccs =
+    let iter =
+      match order with
+      | `topological -> List.iter
+      | `reversed -> fun f l -> List.(iter f (rev l))
+    in
+    iter
+      (fun scc ->
+         let scc = List.(Kernel_function.(scc |> filter is_definition |> map get_definition)) in
+         until_convergence
+           (fun fi ->
+              List.iter
+                (fun d ->
+                   Console.debug "Analysing function %s..." d.svar.vname;
+                   let v = v fi d in
+                   ignore @@ visitFramacFunction (v :> frama_c_visitor) d;
+                   v#finish;
+                   Console.debug ~level:3 "Resulting effect is:@.%a@." I.E.pp @@ I.get fi flag d))
+           fi
+           scc)
+      sccs
+end
