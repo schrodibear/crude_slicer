7fee7de0eaa703568886da31504fd4e511d3edc1
Mikhail Mandrykin <mandrykin@ispras.ru>
2017-10-19 22:37:26 +0300

    Further preparation for SV-COMP

---
+++ wrapper.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+OTHER=()
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        -ldv-bam-svcomp|-sv-comp17-bam-bnb|-skipRecursion|-32|-64|-noout|-stats)
+            OTHER+=("$1")
+            shift
+            ;;
+        -spec)
+            PROPERTYFILE="$2"
+            shift # past argument
+            shift # past value
+            ;;
+        -*)
+            OTHER+=("$1" "$2")
+            shift # past argument
+            shift # past value
+            ;;
+        *)
+            TASK="$1"
+            shift
+            ;;
+    esac
+done
+
+if [[ ! -f "$PROPERTYFILE" ]] || grep -q 'CHECK(.*,.*LTL(G.*!.*call(.*).*).*)' "$PROPERTYFILE"; then
+    if [[ ! -f "$PROPERTYFILE" ]]; then
+        PROPERTYFILE=config/specification/sv-comp-reachability.spc
+    fi
+    STASK="${TASK}.sliced.c"
+    ./frama-c-Crude_slicer.opt \
+        -machdep gcc_x86_64 \
+        -crude_slicer \
+        -timeout 400 \
+        -no-recognize_wrecked_container_of \
+        -widening_threshold 2000 \
+        -no-summaries \
+        -no-assert_stratification \
+        -print \
+        -ocode ${STASK} \
+        ${TASK}
+    if [[ ! -f "${STASK}" ]]; then
+        STASK="${TASK}"
+    fi
+    rm -rf "$(find . -name '**.graphml')"
+    ./scripts/cpa.sh "${OTHER[@]}" -spec "${PROPERTYFILE}" "${STASK}"
+    IWITNESSFILE=$(find . -name "**.graphml")
+    OWITNESSFILE="$(dirname ${IWITNESSFILE})/restored_$(basename ${IWITNESSFILE})"
+    ./filter_witness.native \
+        -progfile "${TASK}" \
+        -o "${OWITNESSFILE}" \
+        "${IWITNESSFILE}"
+    rm -rf "${IWITNESSFILE}"
+else
+    echo "Verification result: UNKNOWN"
+    exit 0
+fi
